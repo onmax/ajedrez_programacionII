12b6c941e8ed550fac7f8cfa1afad2350bb99602
Javitemek <javierbarraganh@gmail.com>
2017-05-08 21:24:22 +0200

    Las fichas chaval

---
+++ src/progii/juegotablero/model/ajedrez/piezas/Reina.java
@@ -0,0 +1,14 @@
+package progii.juegotablero.model.ajedrez.piezas;
+
+import progii.juegotablero.model.Jugador;
+import progii.juegotablero.model.ajedrez.PiezaAjedrez;
+import progii.juegotablero.model.ajedrez.TipoPiezaAjedrez;
+
+public class Reina extends PiezaAjedrez {
+
+	public Reina(Jugador jugador, TipoPiezaAjedrez tipoPieza, int x, int y) {
+		super(jugador, tipoPieza, x, y);
+
+	}
+
+}
