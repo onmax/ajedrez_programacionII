3306f2cc457b8b65cefb74aaf7f8c8f45ef50399
Javitemek <javierbarraganh@gmail.com>
2017-05-12 13:18:59 +0200

    Update Reina.java

--- src/progii/juegotablero/model/ajedrez/piezas/Reina.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Reina.java
@@ -4,46 +4,29 @@ import progii.juegotablero.model.Jugador;
 import progii.juegotablero.model.ajedrez.PiezaAjedrez;
 import progii.juegotablero.model.ajedrez.TipoPiezaAjedrez;
 
-/**
- * Clase que representa a la Reina
- * @author groman
- *
- */
+public class Reina extends PiezaAjedrez {
 
-<<<<<<< HEAD
-public class Reina extends PiezaAjedrez  {
-=======
+	@SuppressWarnings("static-access")
 	public Reina(Jugador jugador, TipoPiezaAjedrez tipoPieza, int x, int y) {
 		super(jugador, tipoPieza.REINA, x, y);
->>>>>>> 3ed4b9f4068432ff42d2026a67a19e214c889173
-
-	/**
-	 * Crea una Reina pertenenciente a jugador en la posición (x,y) del tablero
-	 * @param jugador El jugador al que pertenece la pieza
-	 * @param x Fila que ocupa 
-	 * @param y Columna que ocupa
-	 */
-	public Reina(Jugador jugador, int x, int y) {
-		super(jugador, TipoPiezaAjedrez.REINA, x, y);
-		
 	}
-
 	/**
-	 * Devuelve si la Reina puede mover a la posición (toX,toY) habiendo o no contrario en dicha posición. 
-	 * Se debe comprobar si el movimiento se sale del tablero y para ello se debe usar el método puedeMover de la 
+	 * Devuelve si la Reina puede mover a la posicion (toX,toY) habiendo o no contrario en dicha posiciÃ³n. 
+	 * Se debe comprobar si el movimiento se sale del tablero y para ello se debe usar el mÃ©todo puedeMover de la 
 	 * clase PiezaAjedrez.
 	 * @param toX Fila destino del movimiento 
 	 * @param toY Columna destino del movimiento
-	 * @param hayContrario true si hay una pieza contraria en la posición (toX,toY)
-	 * @return true si el movimiento es válido y false e.o.c.
+	 * @param hayContrario true si hay una pieza contraria en la posiciÃ³n (toX,toY)
+	 * @return true si el movimiento es vÃ¡lido y false e.o.c.
 	 */
-
+	
 	@Override
 	public boolean puedeMover(int toX, int toY, boolean hayContrario) {
-		return super.puedeMover(toX, toY, hayContrario) && 
-				( (this.getX()!=toX && toY == getY()) || 
-				  (this.getY()!= toY && toX == getX()) ); 
+		return (super.puedeMover(toX, toY, hayContrario) && (this.getX() != toX && toY == getY())
+				|| (this.getY() != toY && toX == getX()) || // Movimiento de la
+															// torre
+				(((this.getX() + this.getY()) % 2 == 0) && ((toX + toY) % 2 == 0) // Movimiento
+																					// Alfil
+						|| ((this.getX() + this.getY()) % 2 != 0) && (toX + toY) % 2 != 0));
 	}
-
 }
-
