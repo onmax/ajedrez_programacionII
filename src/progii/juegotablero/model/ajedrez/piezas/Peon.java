ac9bc0aabeae61051bfb76afa339496bb2181c61
Máximo J. García Martínez <onmax@users.noreply.github.com>
2017-05-19 18:35:23 +0200

    Update Peon.java

--- src/progii/juegotablero/model/ajedrez/piezas/Peon.java
+++ src/progii/juegotablero/model/ajedrez/piezas/Peon.java
@@ -3,7 +3,15 @@ import progii.juegotablero.model.Jugador;
 import progii.juegotablero.model.ajedrez.ControlJugadoresAjedrez;
 import progii.juegotablero.model.ajedrez.PiezaAjedrez;
 import progii.juegotablero.model.ajedrez.TipoPiezaAjedrez;
-
+import anotacion.Programacion2;
+@Programacion2 (
+nombreAutor1 = "Maximo",
+apellidoAutor1 = "García Martínez",
+emailUPMAutor1 = "maximo.garcia.martinez@alumnos.upm.es",
+nombreAutor2 = "Javier",
+apellidoAutor2 = "Barragán Haro",
+emailUPMAutor2 = "javier.barragan.haro@alumnos.upm.es"
+)
 /**
  * Clase que representa a la Peon
  * @author groman
@@ -36,20 +44,20 @@ public class Peon extends PiezaAjedrez  {
 	@Override
 	public boolean puedeMover(int toX, int toY, boolean hayContrario) {
 		boolean negro = this.getJugador().getId() == ControlJugadoresAjedrez.NEGRO;
+		boolean puede = super.puedeMover(toX, toY, hayContrario);
 		if(negro){
 			if(this.getX() == 1){
-				return super.puedeMover(toX, toY, hayContrario) && (((this.getX() + 2) == toX || (this.getX() + 1)== toX) && this.getY() == toY && !hayContrario) || hayContrario && (this.getX() + 1 == toX && ((this.getY() + 1 == toY)||(this.getY() - 1 == toY))); 
+				return puede && (((getX()+1 == toX)||(getX()+2 == toX)) && getY() == toY && !hayContrario) || (hayContrario && (getX() + 1 == toX) && ((getY()-1==toY)||(getY() + 1==toY)));
 			}else{
-				return super.puedeMover(toX, toY, hayContrario) && (((this.getX() + 1) == toX) && this.getY() == toY && !hayContrario) || hayContrario && (this.getX() + 1 == toX && ((this.getY() + 1 == toY)||(this.getY() - 1 == toY))); 
+				return puede && ((getX()+1 == toX) && getY() == toY && !hayContrario) || (hayContrario && (getX() + 1 == toX) && ((getY()-1==toY)||(getY() + 1==toY)));
 			}
 		}else{
 			if(this.getX() == 6){
-				return super.puedeMover(toX, toY, hayContrario) && (((this.getX() - 2) == toX || (this.getX() - 1)== toX) && this.getY() == toY && !hayContrario) || hayContrario && (this.getX() - 1 == toX && ((this.getY() + 1 == toY)||(this.getY() - 1 == toY))); 
+				return puede && (((getX()-1 == toX)||(getX()-2 == toX)) && getY() == toY && !hayContrario) || (hayContrario && (getX() - 1 == toX) && ((getY()-1==toY)||(getY() + 1==toY)));
 			}else{
-				return super.puedeMover(toX, toY, hayContrario) && (((this.getX() - 1) == toX) && this.getY() == toY && !hayContrario) || hayContrario && (this.getX() - 1 == toX && ((this.getY() + 1 == toY)||(this.getY() - 1 == toY))); 
+				return puede && ((getX()-1 == toX) && getY() == toY && !hayContrario) || (hayContrario && (getX() - 1 == toX) && ((getY()-1==toY)||(getY() + 1==toY)));
 			}
 		}
 	}
 
 }
-
